1f5cbcf8dd747109d5ee3b4c6c844baf7db43ae5
Oseni <oseniayomide75@gmail.com>
2022-09-01 13:37:53 -0700

    Scraper added

--- news/views.py
+++ news/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views import generic
 
+from .scraper import news_scraper
+
 # Create your views here.
 class HomeView(generic.TemplateView):
   template_name = "news/index.html"
+  
+  def get_context_data(self, *args, **kwargs):
+    context = super(HomeView, self).get_context_data(*args, **kwargs)
+    context["news"] = news_scraper()
+    return context
