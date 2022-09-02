cd9bb8ab2f2fc1b4e2fa8e3cc64eeb04accfd64b
Oseni03 <oseniayomide57@gmail.com>
2022-09-02 14:40:16 +0100

    Htmx poll implemented

--- news/views.py
+++ news/views.py
@@ -7,6 +7,12 @@ from .scraper import latest
 class HomeView(generic.TemplateView):
   template_name = "news/index.html"
   
+  def get(self, *args, **kwargs):
+    if self.request.htmx:
+      context = {"news": latest()}
+      return render(self.request, "news/partials/news_list.html", context)
+    return super().get(*args, **kwargs)
+  
   def get_context_data(self, *args, **kwargs):
     context = super(HomeView, self).get_context_data(*args, **kwargs)
     context["news"] = latest()
